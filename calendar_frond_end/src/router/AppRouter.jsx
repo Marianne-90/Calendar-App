ee2c2e6d5a78f65a3e395456bc17271de7a7b201
Marianne Garrido <marianne.garridom@gmail.com>
2023-10-02 16:52:30 -0600

    variables de entorno

--- calendar_frond_end/src/router/AppRouter.jsx
+++ calendar_frond_end/src/router/AppRouter.jsx
@@ -1,21 +1,21 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { LoginPage } from '../auth';
-import { CalendarPage } from '../calendar';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { LoginPage } from "../auth";
+import { CalendarPage } from "../calendar";
+import { getEnvVariables } from "../helpers";
 
 export const AppRouter = () => {
+  const authStatus = "authenticated";
+  // console.log(getEnvVariables());
 
-const authStatus = 'authenticated'
-  
   return (
     <Routes>
-      {
-        ( authStatus === 'not-authenticated' )
-        ? <Route path="/auth/*" element = {<LoginPage/>}/>
-        :<Route path="/*" element = {<CalendarPage/>}/>
-      }
-      
-      <Route path="/*" element = {<Navigate to='auth/login'/>} />
-      
+      {authStatus === "not-authenticated" ? (
+        <Route path="/auth/*" element={<LoginPage />} />
+      ) : (
+        <Route path="/*" element={<CalendarPage />} />
+      )}
+
+      <Route path="/*" element={<Navigate to="auth/login" />} />
     </Routes>
-  )
-}
+  );
+};
