330193f40c44521f54f333e2743d7eb6e5ac53bb
Marianne Garrido <marianne.garridom@gmail.com>
2023-10-04 17:57:04 -0600

    autenticación

--- calendar_frond_end/src/api/calendarApi.js
+++ calendar_frond_end/src/api/calendarApi.js
@@ -6,6 +6,14 @@ const { VITE_API_URL } = getEnvVariables();
 const calendarApi = axios.create({
   baseURL: VITE_API_URL,
 });
+calendarApi.interceptors.request.use((config) => {
 
+  config.headers = {
+    ...config.headers,
+    'x-token': localStorage.getItem('token')
+  }
 
-export default calendarApi; 
+  return config;
+});
+
+export default calendarApi;
--- calendar_frond_end/src/hooks/useAuthStore.js
+++ calendar_frond_end/src/hooks/useAuthStore.js
@@ -22,24 +22,47 @@ export const useAuthStore = () => {
     }
   };
 
-  const startRegister = async ( { name, email, password }) => {
-    console.log('startRegister');
+  const startRegister = async ({ name, email, password }) => {
+    console.log("startRegister");
     dispatch(onChecking());
 
     try {
-      const { data } = await calendarApi.post("/auth/new", { name, email, password });
+      const { data } = await calendarApi.post("/auth/new", {
+        name,
+        email,
+        password,
+      });
       localStorage.setItem("token", data.token);
       localStorage.setItem("token-init-date", new Date().getTime());
       dispatch(onLogin({ name: data.name, uid: data.uid }));
     } catch (error) {
       console.log(error.response.data);
 
-      dispatch(onLogOut(error.response.data?.msg ||'error al crear usuario'));
+      dispatch(onLogOut(error.response.data?.msg || "error al crear usuario"));
       setTimeout(() => {
         dispatch(clearErrorMessage());
       }, 10);
     }
+  };
 
+  const checkAuthToken = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return dispatch(onLogOut());
+
+    try {
+      const { data } = await calendarApi.get("auth/renew");
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
+    } catch (error) {
+      localStorage.clear();
+      dispatch(onLogOut());
+    }
+  };
+
+  const startLogOut = () => {
+    localStorage.clear();
+    dispatch(onLogOut());
   };
 
   return {
@@ -52,5 +75,7 @@ export const useAuthStore = () => {
     //*METODOS
     startLogin,
     startRegister,
+    checkAuthToken,
+    startLogOut,
   };
 };
